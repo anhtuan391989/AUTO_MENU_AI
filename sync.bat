d2f025eb3dea5ff44e92403fd3a076d30b0c649c
anhtuan391989 <matuotluuly@gmail.com>
2026-07-16 15:44:18 +0700

    Auto-sync 16/07/2026 15:44:18,58

--- sync.bat
+++ sync.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "G:\AUTO_MENU_AI"
 
-<<<<<<< HEAD
 :: 1. Lấy code mới nhất từ GitHub về để tránh lỗi "rejected"
 git pull origin main
 
@@ -14,15 +13,3 @@ git push -u origin main
 
 echo Da day du lieu len nhanh main thanh cong!
 timeout /t 3
-=======
-:: Lấy code mới nhất về trước
-git pull origin develop
-
-:: Đẩy code từ máy lên nhánh develop
-git add .
-git commit -m "Auto-sync %date% %time%"
-git push -u origin develop
-
-echo Da day du lieu len nhanh develop!
-timeout /t 5
->>>>>>> 944abbe23990144a62ee0aa6e3ff7d26e87a528b
