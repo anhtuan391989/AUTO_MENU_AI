7c30c733aba58eb27e574fa7357cf234b7dae972
anhtuan391989 <matuotluuly@gmail.com>
2026-07-16 15:39:43 +0700

    Auto-sync 16/07/2026 15:39:43,22

--- sync.bat
+++ sync.bat
@@ -1,13 +1,24 @@
 @echo off
 cd /d "G:\AUTO_MENU_AI"
 
-:: Lấy code mới nhất về trước
+:: 1. Lấy code mới nhất về
 git pull origin develop
 
-:: Đẩy code từ máy lên nhánh develop
+:: 2. Lưu thay đổi của bạn vào nhánh develop
 git add .
 git commit -m "Auto-sync %date% %time%"
-git push -u origin develop
+git push origin develop
 
-echo Da day du lieu len nhanh develop!
+:: 3. Tự động merge develop vào main tại máy tính
+git checkout main
+git pull origin main
+git merge develop -m "Auto-merge develop to main"
+
+:: 4. Đẩy kết quả sau khi đã gộp lên GitHub
+git push origin main
+
+:: 5. Quay lại nhánh develop để tiếp tục làm việc
+git checkout develop
+
+echo Da dong bo tu dong len Main!
 timeout /t 5
