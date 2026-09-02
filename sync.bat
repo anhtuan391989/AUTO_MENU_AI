6bf46c2d79169255e63e77c10c1e626c71096a1e
Khói <matuotluuly@gmail.com>
2026-09-02 16:31:17 +0700

    Auto-sync 02/09/2026 16:31:17,15

--- sync.bat
+++ sync.bat
@@ -6,7 +6,7 @@ git pull origin main
 
 :: 2. Thêm và lưu thay đổi vào nhánh main
 git add .
-git commit -m "Up-Date lần cuối: %date% %time%"
+git commit -m "Auto-sync %date% %time%"
 
 :: 3. Đẩy thẳng lên nhánh main trên GitHub
 git push -u origin main
